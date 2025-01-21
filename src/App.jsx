13648064b16ddaab6b40db24ba29bf44c248ffee
abhishek <abhishekparayil01@gmail.com>
2025-01-21 12:51:44 +0530

    globe trek

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <p className='text-2xl font-bold text-red-500 text-center'>HELLO WORLD</p>
+    </>
+  )
+}
+
+export default App
